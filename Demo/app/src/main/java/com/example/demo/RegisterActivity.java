ecae4826f68f45cc32de3ac7f793c80c3b709ab7
TruongHongAnh <truonghonganh22102005@gmail.com>
2025-09-27 19:39:44 +0700

    Push file code trang đăng ký

---
+++ Demo/app/src/main/java/com/example/demo/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edtUsername, edtEmailRegister, edtPasswordRegister, edtConfirmPassword;
+    Button btnRegister;
+    TextView txtLoginNow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtEmailRegister = findViewById(R.id.edtEmailRegister);
+        edtPasswordRegister = findViewById(R.id.edtPasswordRegister);
+        edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+}
