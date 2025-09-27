2108147fbf37aed78ced30bec8f378c198027314
TruongHongAnh <truonghonganh22102005@gmail.com>
2025-09-27 19:37:34 +0700

    Sửa code trong file MainActivity để hoạt động hành động chuyển tiếp của trang 1 và 2

--- Demo/app/src/main/java/com/example/demo/MainActivity.java
+++ Demo/app/src/main/java/com/example/demo/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         txtRegisterNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
     }
